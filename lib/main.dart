b9d6d47c3670e46f2caa2c0bd667349d2c235bd7
KauaHenSilva <kaua.sbc@gmail.com>
2024-08-13 16:02:59 -0300

    feat: Add multi-language support for the app and providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'package:dart_flutter_maps/providers/great_places.dart';
 import 'package:dart_flutter_maps/screens/place_form_screen.dart';
 import 'package:dart_flutter_maps/screens/places_list_screen.dart';
 import 'package:dart_flutter_maps/utils/my_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:form_builder_validators/localization/l10n.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => GreatPlaces()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,6 +27,15 @@ class MyApp extends StatelessWidget {
       title: 'Maps',
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        FormBuilderLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('pt', 'BR'),
+      ],
       initialRoute: MyRoutes.placesList,
       routes: {
         MyRoutes.placesList: (ctx) => const PlacesListScreen(),
